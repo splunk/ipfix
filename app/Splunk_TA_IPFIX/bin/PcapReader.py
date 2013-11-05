d668ad607c7961dd5bf4a41a496a5122169a4732
Joel Bennett <JBennett@Splunk.com>
2013-11-05 18:33:28 -0500

    Reimplement parsing for PCAP dumps (note: not pcapNG, old-fashioned pcap)

---
+++ app/Splunk_TA_IPFIX/bin/PcapReader.py
@@ -0,0 +1,47 @@
+from struct import unpack
+
+# modified from scapy
+class PcapReader:
+    def __init__(self, filename):
+        self.f = open(filename, "rb")
+        magic = self.f.read(4)
+        if magic == "\xa1\xb2\xc3\xd4": #big endian
+            self.endian = ">"
+        elif magic == "\xd4\xc3\xb2\xa1": #little endian
+            self.endian = "<"
+        else:
+            raise Exception("Not a pcap capture file (bad magic):{0}".format(magic.encode('hex')))
+
+        hdr = self.f.read(20)
+        if len(hdr)<20:
+            raise Exception("Invalid pcap file (too short)")
+
+        # assume layer2 is Ethernet
+        #vermaj,vermin,tz,sig,snaplen,linktype = struct.unpack(self.endian+"HHIIII",hdr)
+        #self.linktype = linktype
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        """impliment the iterator protocol on a set of packets in a pcap file"""
+        pkt = self.read_packet()
+        if pkt == None:
+            raise StopIteration
+        return pkt
+
+    def read_packet(self):
+        """return a single packet read from the file
+
+        returns None when no more packets are available
+        """
+        hdr = self.f.read(16)
+        if len(hdr) < 16:
+            return None
+        sec,usec,caplen,wirelen = unpack(self.endian+"IIII", hdr)
+        s = self.f.read(caplen)
+        return s
+        #return s,(sec,usec,wirelen) # caplen = len(s)
+
+    def close(self):
+        return self.f.close()
---
+++ app/Splunk_TA_IPFIX/bin/ProcessPcap.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+__author__ = 'Joel Bennett'
+
+from os import path, environ
+import sys
+from struct import unpack
+from ConfigParser import ConfigParser
+from PcapReader import PcapReader
+
+from IPFIX import *
+from SplunkLogger import *
+
+
+## For testing purposes. The following THREE lines.
+if not "SPLUNK_HOME" in environ:
+    APP_PATH = path.abspath('..')
+else:
+    APP_PATH = path.join(environ["SPLUNK_HOME"], 'etc', 'apps', 'Splunk_TA_IPFIX')
+CONFIG_FILE = path.join(APP_PATH, 'default', 'ipfix.conf'), path.join(APP_PATH, 'local', 'ipfix.conf')
+LOG_FILENAME = path.join(APP_PATH, 'log', 'appflow.log')
+DEBUG_LOG_FILENAME = path.join(APP_PATH, 'log', 'debug.log')
+
+# For testing purpose
+#CONFIG_FILE = './ipfix.conf'
+#LOG_FILENAME = './appflow.log'
+
+# Read config file
+Config = ConfigParser()
+Config.read(CONFIG_FILE)
+PROTOCOL = 'pcap'
+# HOST = Config.get('network', 'host')
+PORT = Config.getint('network', 'port')
+# PROTOCOL = Config.get('network', 'protocol')
+MAX_BYTES = Config.getint('logging', 'maxBytes')
+BACKUP_COUNT = Config.getint('logging', 'backupCount')
+
+splunkLogger = SplunkLogger(LOG_FILENAME, MAX_BYTES, BACKUP_COUNT)
+debugLogger = SplunkLogger(DEBUG_LOG_FILENAME, MAX_BYTES, BACKUP_COUNT)
+
+# Currently, only support UDP
+captureFile = Config.get('testing', 'file')
+pkts = PcapReader(captureFile)
+
+# For each packet in the pcap file, extract, decode and print AppFlow IPFIX records.
+for p in pkts:
+    # assume layer 2 is Ethernet
+    l3type = unpack(">H", p[12:14])[0]
+    if l3type != 0x800:  # not IP
+        debugLogger.info("DISCARD: Non-IP Packet")
+        continue
+
+    pos = 14  # Ethernet length
+    tmp = ord(p[pos])
+    ip_version = tmp >> 4
+    ip_hdr_len = (tmp & 0x0F) << 2
+    l4type = ord(p[pos + 9])
+    if ip_version != 4 or l4type != 17:  # not ipv4 or UDP
+        debugLogger.info("DISCARD: Non-IP4 Packet (or non-UDP packet")
+        continue
+
+    pos += ip_hdr_len
+    src_port, dst_port, udp_len = unpack(">HHH", p[pos:pos + 6])
+    data = p[pos + 8:]
+    if len(data) != udp_len - 8:  # data length does not equal length in UDP header
+        debugLogger.info("DISCARD: bad length UDP packet (header: {0}, actual: {1}".format(udp_len, len(data)))
+        continue
+
+    if dst_port == PORT:
+        addr = ['unknown', src_port]
+        ipfix = Parser(data, addr, logger=debugLogger)
+        if ipfix.data:
+            splunkLogger.info(str(ipfix))
+
+    else:
+        debugLogger.info("DISCARD: Data to wrong port {0} observer='{observer}' data='{data}'".format(dst_port, src_port, data.encode('hex')))
