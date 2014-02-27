81bcd0eafc262437141540cdb57cb2096c7b7615
Joel Bennett <JBennett@Splunk.com>
2014-02-27 13:33:02 -0500

    Clean up validation error messages

--- app/Splunk_TA_IPFIX/bin/IPFIX/information-elements/Validate.py
+++ app/Splunk_TA_IPFIX/bin/IPFIX/information-elements/Validate.py
@@ -10,8 +10,6 @@ __author__ = 'JBennett'
 
 import lxml.etree as et
 import os, re
-from splunk.appserver.mrsparkle.lib.util import make_splunkhome_path
-
 
 FOLDER = os.path.dirname(os.path.abspath(__file__))
 
@@ -24,13 +22,14 @@ for root, dirs, files in os.walk(FOLDER):
     for name in files:
         if name.endswith('.xml'):
             print '=' * 80
-            print "Validating file: %s" % os.path.join(root, name)
-            f = open(os.path.join(root, name), 'r')
+            fullname = os.path.join(root, name).replace("\\","/")
+            print "Validating file: %s" % fullname
+            f = open(fullname, 'r')
             rootNode = et.parse(f)
             isValid = relaxng.validate(rootNode)
             if not isValid:
                 print "Validation error: %s" % name
-                print relaxng.error_log
+                print str(relaxng.error_log).replace("file:///" + fullname + ":","")
                 print ''
             else:
                 print "Valid file: %s" % name
